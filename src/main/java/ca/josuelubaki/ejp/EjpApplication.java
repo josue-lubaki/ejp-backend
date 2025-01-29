37e90f0205d1328cd492e48e6a49fc81d949d9db
Josue Lubaki <josuelubaki@gmail.com>
2025-01-29 02:19:38 -0500

    initial commit

---
+++ src/main/java/ca/josuelubaki/ejp/EjpApplication.java
@@ -0,0 +1,13 @@
+package ca.josuelubaki.ejp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/josuelubaki/ejp/EjpApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.josuelubaki.ejp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
